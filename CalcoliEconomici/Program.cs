dff1d627711a642a0dd0404b63f6a086acae5e2d
sanpydid <90097340+sanpydid@users.noreply.github.com>
2022-09-08 15:00:47 +0200

    pushUpdate README.md

--- CalcoliEconomici/Program.cs
+++ CalcoliEconomici/Program.cs
@@ -5,19 +5,20 @@
         static void Main(string[] args) // entry point
         {
             // MODELLO DEI DATI - VARIABILI 
-            decimal PU, PF = 0m, sconto = 0m, perSconto, IVA;
+            decimal PU, perSconto, IVA;
             int N;
+            decimal PF = 0m, sconto = 0m;
             // ALGORITMO
             Console.Write("Prezzo unitario in euro = "); // INPUT
             PU = decimal.Parse(Console.ReadLine());
             Console.Write("Numero di prodotti venduti = ");
             N = int.Parse(Console.ReadLine());
             Console.Write("Sconto in % da applicare = ");
-            sconto = decimal.Parse(Console.ReadLine());
+            perSconto = decimal.Parse(Console.ReadLine());
             Console.Write("IVA in % da applicare = ");
             IVA = decimal.Parse(Console.ReadLine());
             PF = PU * N; // ELABORAZIONE
-            sconto = PF * sconto / 100m;
+            sconto = PF * perSconto / 100m;
             PF = PF - sconto;
             PF = PF + PF * IVA / 100m;
             Console.WriteLine($"Sconto applicato = {sconto:N2} euro"); // OUTPUT
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AppConsole
 Progetto App Console per calcoli economici
-#PROBLEMA: 
-calcolare il prezzo finale di un certo prodotto, 
-dati il numero di pezzi e il prezzo unitario, conoscendo la percentuale degli eventuali sconti e dell’IVA (Imposta Valore Aggiunto).
+PROBLEMA: 
+calcolare il prezzo finale di un certo prodotto, dati il numero di pezzi e il prezzo unitario,
+conoscendo la percentuale degli eventuali sconti e dell’IVA (Imposta Valore Aggiunto).
